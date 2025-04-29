3d0b3c9fb7b9d87dc17de2b1b341c90ba3fffdb7
Snootic <snooticonoob@hotmail.com>
2025-04-29 09:34:43 -0300

    refactor: update users and racetracks model's attributes

---
+++ models/Autodromo.ts
@@ -0,0 +1,12 @@
+export interface Autodromo{
+    id: string;
+    nome: string;
+    tamanho: number;
+    tipo_pista: string;
+    principais_eventos?: string[];
+    infraestrutura?: string[];
+    localizacao: string;
+    destaque?: string;
+    imagem?: string;
+    descricao?: string
+}
