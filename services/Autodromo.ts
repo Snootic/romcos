cdbe670d2828ff66477e75df802a6bee6555bd1a
Snootic <snooticonoob@hotmail.com>
2025-04-29 09:33:47 -0300

    feat: implement simple crud to manage racetrack and users

--- services/Autodromo.ts
+++ services/Autodromo.ts
@@ -1,2 +1,30 @@
 import { readFile, writeFile } from 'react-native-fs';
 import { join } from 'path';
+import { Autodromo } from '../models/Autodromo';
+
+const AUTODROMO_FILE_PATH = join('/data/', 'autodromo.json');
+
+export const getAutodromo = async (): Promise<Autodromo[]> => {
+  try {
+    const data = await readFile(AUTODROMO_FILE_PATH);
+    return JSON.parse(data);
+  } catch (e) {
+    console.error('Erro ao ler informações dos Autodromos:', e);
+    return [];
+  }
+};
+
+export const saveAutodromo = async (carros: Autodromo[]): Promise<void> => {
+  try {
+    const data = JSON.stringify(carros, null, 2);
+    await writeFile(AUTODROMO_FILE_PATH, data, 'utf8');
+  } catch (e) {
+    console.error('Erro ao salvar informações do autodromo:', e);
+  }
+};
+
+export const addAutodromo = async (autodromo: Autodromo): Promise<void> => {
+  const autodromos = await getAutodromo();
+  autodromos.push(autodromo);
+  await saveAutodromo(autodromos);
+};
--- services/Usuario.ts
+++ services/Usuario.ts
@@ -1,2 +1,30 @@
 import { readFile, writeFile } from 'react-native-fs';
 import { join } from 'path';
+import { Usuario } from '../models/Usuario';
+
+const USUARIO_FILE_PATH = join('/data/', 'usuarios.json');
+
+export const getUsuarios = async (): Promise<Usuario[]> => {
+  try {
+    const data = await readFile(USUARIO_FILE_PATH);
+    return JSON.parse(data);
+  } catch (e) {
+    console.error('Erro ao ler informações dos Usuarios:', e);
+    return [];
+  }
+};
+
+export const saveUsuarios = async (Usuarios: Usuario[]): Promise<void> => {
+  try {
+    const data = JSON.stringify(Usuarios, null, 2);
+    await writeFile(USUARIO_FILE_PATH, data, 'utf8');
+  } catch (e) {
+    console.error('Erro ao salvar informações dos usuarios:', e);
+  }
+};
+
+export const addUsuario = async (Usuario: Usuario): Promise<void> => {
+  const usuarios = await getUsuarios();
+  usuarios.push(Usuario);
+  await saveUsuarios(usuarios);
+};
