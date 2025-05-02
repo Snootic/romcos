c116325ac80c2fb819f618480c452cc332dcd8e0
Snootic <snooticonoob@hotmail.com>
2025-05-01 21:50:21 -0300

    feat: add DetailStyles for consistent styling in details component

---
+++ app/components/styles/details.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const DetailStyles = StyleSheet.create({
+    descricao: {
+      fontSize: 12,
+      color: '#333',
+      flexWrap: 'wrap',
+      width: '80%',
+      marginLeft: '10%',
+      textAlign: 'left',
+    }
+});
+
+export default DetailStyles;
