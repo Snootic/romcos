22a40dbfafa628618db2d16a2d624b4c51a6af69
Snootic <snooticonoob@hotmail.com>
2025-05-01 21:51:47 -0300

    feat: refactor TrackSelection component to use ScrollView and improve layout for autodromo selection

--- app/trackSelection.tsx
+++ app/trackSelection.tsx
@@ -4,19 +4,37 @@ import Menu from './components/menu';
 import DefaultStyles from './components/styles/default';
 import ImageStyles from './components/styles/images';
 
-import EntityView from './components/entities';
-
 import { Autodromo } from '../models/Autodromo';
 
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, ScrollView, Pressable } from 'react-native';
+
+import data from './dev/customData';
+import { AutodromoDetails } from './components/entities';
 
 const TrackSelection = () => {
+    const { AUTODROMO_DATA } = data;
+
     return (
-        <View style={DefaultStyles.container}>
+        <ScrollView contentContainerstyle={DefaultStyles.container}>
             <View style={DefaultStyles.header}>
                 <Text style={DefaultStyles.titulo}>Selecione o Autódromo</Text>
             </View>
-        </View>
+            <FlatList style={{marginTop: -120}} //passando marginTop aqui porque passando diretamente na imagem
+            // o header acaba a sobrepondo, deixando ela recortada
+                data={AUTODROMO_DATA}
+                renderItem={AutodromoDetails}
+                keyExtractor={(item) => item.id}
+                horizontal = { true }
+                showsHorizontalScrollIndicator={true}
+            />
+            <Pressable onPress={() => alert('Autódromo selecionado')} style={DefaultStyles.buttonOne}>
+                <Text style={DefaultStyles.buttonOneText}>Selecionar Autódromo</Text>
+            </Pressable>
+            <Pressable onPress={() => alert('INfos')} style={DefaultStyles.buttonTwo}>
+                <Text style={DefaultStyles.buttonTwoText}>Mais Informações</Text>
+            </Pressable>
+            <Menu/>
+        </ScrollView>
     )
 }
 
