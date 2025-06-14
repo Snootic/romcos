90a23feaa337f81f8f88cd913fa2526d21901abc
Snootic <snooticonoob@hotmail.com>
2025-06-13 23:55:55 -0300

    feat: refactor UserSchedules component to improve layout and integrate pickers for car and track data

--- app/userSchedules.tsx
+++ app/userSchedules.tsx
@@ -5,8 +5,6 @@ import DefaultStyles from './components/styles/default';
 import { View, Text, Pressable, StyleSheet, Platform, ScrollView } from 'react-native';
 import { CustomDatePicker, TimePicker, NativeDatePicker, NativeTimePicker } from './components/customDatePicker';
 import { useNavigation } from '@react-navigation/native';
-import { Autodromo } from '../models/Autodromo';
-import { Carro } from '../models/Carro';
 import CustomData from './dev/customData';
 import { TinyView } from './components/entities';
 
@@ -14,13 +12,15 @@ export default function UserSchedules() {
     const navigation = useNavigation();
     const selectedCar = CustomData.CARRO_DATA[0];
     const selectedTrack = CustomData.AUTODROMO_DATA[0];
+    const carros = CustomData.CARRO_DATA.slice(1, 10);
+    const autodromos = CustomData.AUTODROMO_DATA.slice(1, 10);
 
     let pickers
     if (Platform.OS === 'web') {
         pickers = (
             <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', paddingRight:10, zIndex: 100 }}>
-                <CustomDatePicker selected={new Date()} onChange={() => {}} isClearable={false} readonly={true} />
-                <TimePicker selected={new Date()} onChange={() => {}} isClearable={false} readonly={true} />
+                <CustomDatePicker selected={new Date(2025, 5, 13)} onChange={() => {}} isClearable={false} readonly={true} />
+                <TimePicker selected={new Date(2025, 5, 13, 14, 0)} onChange={() => {}} isClearable={false} readonly={true} />
             </View>
         )
     }
@@ -32,18 +32,56 @@ export default function UserSchedules() {
             </View>
         )
     }
+
+    const maxLength = Math.max(carros.length, autodromos.length);
+    const intercalados = [];
+    for (let i = 0; i < maxLength; i++) {
+        intercalados.push(
+            <Text style={[DefaultStyles.textBold, {color: 'black'}]}>Pedido {i+1}</Text>
+        )
+        if (carros[i]) {
+            intercalados.push(
+                <View style={styles.itemBox} key={`carro-${carros[i].id || i}`}>
+                    <View style={[styles.elementsBox, {width: '40%'}]}>
+                        <TinyView item={carros[i]} />
+                    </View>
+                    <View style={[styles.elementsBox, {width: '60%'}]}>
+                        <Text style={DefaultStyles.textBold}>Data de retirada</Text>
+                        {pickers}
+                        <Text style={DefaultStyles.textBold}>Data de Devolução</Text>
+                        {pickers}
+                    </View>
+                </View>
+            );
+        }
+        if (autodromos[i]) {
+            intercalados.push(
+                <View style={styles.itemBox} key={`autodromo-${autodromos[i].id || i}`}>
+                    <View style={[styles.elementsBox, {width: '40%'}]}>
+                        <TinyView item={autodromos[i]} />
+                    </View>
+                    <View style={[styles.elementsBox, {width: '60%'}]}>
+                        <Text style={DefaultStyles.textBold}>Data de retirada</Text>
+                        {pickers}
+                        <Text style={DefaultStyles.textBold}>Data de Devolução</Text>
+                        {pickers}
+                    </View>
+                </View>
+            );
+        }
+    }
     return (
         <><ScrollView contentContainerStyle={DefaultStyles.container}>
             <Header title="Seus agendamentos" />
-            <Text style={[DefaultStyles.titleText, {marginTop: -130, color: 'black'}]}>Em andamento</Text>
             <Pressable 
-                style={styles.boxContainer}
+                style={[styles.boxContainer, {marginTop: -130}]}
                 onPress={() => navigation.navigate('schedule')}>
+                <Text style={[DefaultStyles.titleText, {color: 'black'}]}>Em andamento</Text>
                 <View style={styles.itemBox}>
-                    <View style={styles.elementsBox}>
+                    <View style={[styles.elementsBox, {width: '40%'}]}>
                         <TinyView item={selectedCar} />
                     </View>
-                    <View style={styles.elementsBox}>
+                    <View style={[styles.elementsBox, {width: '60%'}]}>
                         <Text style={DefaultStyles.textBold}>Data de retirada</Text>
                         {pickers}
                         <Text style={DefaultStyles.textBold}>Data de Devolução</Text>
@@ -51,10 +89,10 @@ export default function UserSchedules() {
                     </View>
                 </View>
                 <View style={styles.itemBox}>
-                    <View style={styles.elementsBox}>
+                    <View style={[styles.elementsBox, {width: '40%'}]}>
                         <TinyView item={selectedTrack} />
                     </View>
-                    <View style={styles.elementsBox}>
+                    <View style={[styles.elementsBox, {width: '60%'}]}>
                         <Text style={DefaultStyles.textBold}>Data de retirada</Text>
                         {pickers}
                         <Text style={DefaultStyles.textBold}>Data de Devolução</Text>
@@ -62,7 +100,11 @@ export default function UserSchedules() {
                     </View>
                 </View>
             </Pressable>
-            <Text style={[DefaultStyles.titleText, {color: 'black', marginTop: 40}]}>Finalizados</Text>
+            <Pressable 
+                style={[styles.boxContainer, {marginTop: 70}]}>
+                <Text style={[DefaultStyles.titleText, {color: 'black'}]}>Finalizados</Text>
+                {intercalados}
+            </Pressable>
         </ScrollView>
         <Menu /></>
     )
@@ -72,7 +114,7 @@ const styles = StyleSheet.create({
     boxContainer: {
         borderRadius: 20, 
         width: '90%',
-        height: '65%',
+        height: '20%',
         // marginTop: -120,
         alignSelf: 'center',
         display: 'flex',
